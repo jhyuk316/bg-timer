4c8574a7abfd81aad8bdeb1d7a95d80e216945a9
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2026-09-23 03:55:53 +0900

    Allow five seconds before lobby expiry

--- js/multiplayer/constants.js
+++ js/multiplayer/constants.js
@@ -4,4 +4,4 @@ export const MAX_PLAYERS = 6;
 export const ROOM_STORAGE_KEY = 'bg-timer-room';
 export const ROOM_CODE_LENGTH = 6;
 export const PRESENCE_HEARTBEAT_MS = 1_000;
-export const LOBBY_STALE_MS = 3_000;
+export const LOBBY_STALE_MS = 5_000;
